2847bda67c3ba551fbc49b9500277474adf05e01
VanesaJuarez13 <marvyk.mayoral@udgvirtual.udg.mx>
2024-12-01 19:25:18 -0600

    Corrige configuración para despliegue en Render

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 # Configuración de la aplicación Flask
 app = Flask(__name__)
@@ -21,19 +22,16 @@ class Record(db.Model):
 # Rutas
 @app.route('/')
 def index():
-    """Página principal que lista todos los registros"""
     records = Record.query.all()
     return render_template('index.html', records=records)
 
 @app.route('/record/<int:id>')
 def details(id):
-    """Detalles de un registro individual"""
     record = Record.query.get_or_404(id)
     return render_template('details.html', record=record)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
-    """Agregar un nuevo registro"""
     if request.method == 'POST':
         name = request.form['name']
         description = request.form['description']
@@ -46,7 +44,6 @@ def add():
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
-    """Editar un registro existente"""
     record = Record.query.get_or_404(id)
     if request.method == 'POST':
         record.name = request.form['name']
@@ -58,7 +55,6 @@ def edit(id):
 
 @app.route('/delete/<int:id>', methods=['POST'])
 def delete(id):
-    """Eliminar un registro"""
     record = Record.query.get_or_404(id)
     db.session.delete(record)
     db.session.commit()
@@ -71,4 +67,5 @@ with app.app_context():
 
 # Iniciar la aplicación
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
